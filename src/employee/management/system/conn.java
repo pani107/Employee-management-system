f32a76d9239a5b884753a759e41e1a20b949443e
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-08-31 20:39:47 +0530

    Update conn.java
    add comment.

--- src/employee/management/system/conn.java
+++ src/employee/management/system/conn.java
@@ -4,16 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class conn {
+public class conn { //connection class
 
     Connection connection;
     Statement statement;
 
     public conn(){
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeemanegementsystem","root","pani@107");
-            statement = connection.createStatement();
+            Class.forName("com.mysql.cj.jdbc.Driver");// load driver
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeemanegementsystem","root","pani@107");//actual connection
+            statement = connection.createStatement();// statement creation
 
         }catch (Exception e){
             e.printStackTrace();
